a714a84db82d1bc03409f44d657f6a7ee22d5909
Phan Thanh Huy <phanthanhhuyhd21@gmail.com>
2025-08-06 20:28:10 +0700

    Update attendance_db.sql

--- DiemDanh_App/attendance_db.sql
+++ DiemDanh_App/attendance_db.sql
@@ -48,9 +48,9 @@ INSERT INTO student (mssv, hoten, lop, email) VALUES
 INSERT INTO login (maso, password) VALUES
 ('B2204933', '$2a$10$D5iT1eyqYPZJnG0ULlmhAuAAu7L3w/OSCZ5LO4k/mE6.vJ7h7LjSm'),
 ('B1234567', '$2a$10$ia087ALHGZX9oJ7.hs0oFumxnryJTolSz50DZ0fTx3eYIYlYHod4e'),
-('B2222222', '$2a$10$gkRK/x2jYoc2qcny3elWNeMHHKPAmXHCz1Qt10FWDuoLYcMhgs9QG'),
 ('000111', '$2a$10$P/y4QzfvkuWgJB0Ohru.UurjGhL2CxooJ9.QJhzHG.LxCbwLaU.SS');
 
 INSERT INTO hocphan (maHP, tenHP, namhoc, hocki, nhom) VALUES 
 ('CT221', 'Lập trình mạng', '2024-2025', 1, 1),
 ('CT233', 'Điện toán đám mây', '2024-2025', 1, 1);
+
