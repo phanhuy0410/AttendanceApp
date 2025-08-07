efadd6860af6431645c881423302e4a9fa465649
Phan Thanh Huy <phanthanhhuyhd21@gmail.com>
2025-08-07 21:20:12 +0700

    Add files via upload

---
+++ DiemDanh_App/teacher/LoginController.java
@@ -0,0 +1,65 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import org.mindrot.jbcrypt.BCrypt;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+public class LoginController {
+    @FXML
+    private TextField teacherIdField;
+    @FXML
+    private PasswordField passwdField;
+    @FXML
+    private Button loginButton;
+    
+    @FXML
+    public void Login(ActionEvent event) {
+        String msgv = teacherIdField.getText();
+        String password = passwdField.getText();
+        if (msgv.isEmpty() || password.isEmpty()) {
+            showAlert(Alert.AlertType.INFORMATION, "Thông báo", "Vui lòng nhập đầy đủ thông tin!");
+            return;
+        }
+        try (java.sql.Connection conn = DBHelper.connect()) {
+            String sql = "SELECT password FROM login WHERE maso = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, msgv);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                String storedHash = rs.getString("password");
+                if (BCrypt.checkpw(password, storedHash)) {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("teacher_view.fxml"));
+                    Parent root = loader.load();
+                    TeacherController controller = loader.getController();
+                    controller.setTeacherInfo(msgv);
+                    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    stage.setScene(new Scene(root));
+                    stage.show();
+                } else {
+                    showAlert(Alert.AlertType.WARNING, "Lỗi", "Sai mật khẩu");
+                }
+            } else {
+                showAlert(Alert.AlertType.WARNING, "Cảnh báo", "Không tìm thấy MSGV!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+}
